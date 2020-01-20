1957d8d6cfeb6ed767eb97c6982a6b1ac2a13a23
Champika <champikadp@gmail.com>
2020-01-20 20:06:59 +1100

    fourth commit

--- script.js
+++ script.js
@@ -4,3 +4,32 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+const generateTarget = () => (Math.floor(Math.random() * 9));
+// console.log(generateTarget());
+
+const compareGuesses = (human, computer, targrt) => {
+    let humanDifference = Math.abs(human - targrt);
+    let computerDifference = Math.abs(computer - targrt);
+
+    if (humanDifference === computerDifference) {
+        return true;
+    } else if (humanDifference > computerDifference) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+const updateScore = winner => {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    if (winner === 'human') {
+        humanScore += 1;
+    } else if (winner === 'computer') {
+        computerScore += 1;
+    }
+};
+
+
+const advanceRound = () => currentRoundNumber += 1;
